c9e048b8de543c89113c74c420100e5f1b79e0aa
Katie Rose <katiefelten@gmail.com>
2019-08-27 12:00:18 -0400

    :zap: adds postgres

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,28 @@
+const localPg = {
+  host: "localhost",
+  database: "auth",
+  user: "student",
+  password: "hired"
+};
+const productionDbConnection = process.env.DATABASE_URL || localPg;
+
 module.exports = {
   development: {
-    client: 'sqlite3',
-    connection: { filename: './database/auth.db3' },
+    client: "sqlite3",
+    connection: { filename: "./database/auth.db3" },
     useNullAsDefault: true,
     migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
+      directory: "./database/migrations",
+      tableName: "dbmigrations"
     },
-    seeds: { directory: './database/seeds' },
+    seeds: { directory: "./database/seeds" }
   },
+  production: {
+    client: "pg",
+    connection: productionDbConnection,
+    migrations: {
+      directory: "./database/migrations"
+    },
+    seeds: { directory: "./database/seeds" }
+  }
 };
